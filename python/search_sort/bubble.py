2ad9c77e67bfe44624cc7d6673c4d305891754fa
Mark <mnkolev@gmail.com>
2018-05-10 16:51:54 -0400

    added problems

---
+++ python/search_sort/bubble.py
@@ -0,0 +1,13 @@
+t=int(input("Test cases: "))
+test_case=0
+num_val=0
+
+while test_case<t:
+    n=int(input("Array size"))
+    while num_val<n:
+    
+        #val=int(input())
+
+        num_val+=1
+
+    test_case+=1
